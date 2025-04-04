269ab543579cef04ac8661b1d07d3f3fd2ea9797
sujal-bansal <bansalsujal2021@gmail.com>
2025-04-04 17:38:01 +0530

    Implemented state management and authorization routes in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: "http://localhost:8000/api",
   withCredentials: true,
 });
