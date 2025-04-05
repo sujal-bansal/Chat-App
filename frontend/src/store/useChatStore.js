59d0ef954f1367b87edebd155c25f0028532c4eb
sujal-bansal <bansalsujal2021@gmail.com>
2025-04-05 11:42:27 +0530

    Update auth store and add chat/theme stores with constants. Also added chat-related components and avatar asset

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,39 @@
+import axiosInstance from "../lib/axios";
+import toast from "react-hot-toast";
+import create from "zustand";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: [],
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  setSelectedUser: (selectedUser) => {
+    set({ selectedUser });
+  },
+}));
